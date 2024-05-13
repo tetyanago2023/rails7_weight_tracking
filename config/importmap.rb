8669f260f8a65334721a18dd37814cd079ef6729
go2023 <tetyanago2018@gmail.com>
2024-05-13 13:39:26 -0700

    ApexCharts was added, weight stimulus controller was created

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "apexcharts" # @3.48.0
+pin "apexcharts", to: "https://cdn.skypack.dev/apexcharts@3.33.1"
